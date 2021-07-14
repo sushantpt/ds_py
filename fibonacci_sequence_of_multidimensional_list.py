5b0ed1af05ce69a92e658311a33733708efe4974
sushantpt <66266432+sushantpt@users.noreply.github.com>
2021-07-14 13:20:50 +0545

    Delete fibonacci_sequence_of_multidimensional_list.py

--- fibonacci_sequence_of_multidimensional_list.py
+++
@@ -1,8 +0,0 @@
-mat = [[1,2,3,4], [3,4,2], [12,44,56], [12,342,23]]
-sums_list = []
-for i in range(len(mat)):
-    total = 0
-    for j in range(len(mat[i])):
-        total = total + mat[i][j]
-        sums_list.append(total)
-print(sums_list)
